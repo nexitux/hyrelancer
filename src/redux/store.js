1e616887be41bed9c1dac78b32d7a82447af4253
mail2mathewpeter <123640890+mail2mathewpeter@users.noreply.github.com>
2025-09-22 13:29:27 +0530

    customer post,list

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import adminReducer from './slices/adminSlice';
+import jobReducer from './slices/jobSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     admin: adminReducer,
+    job: jobReducer,
   },
 });
